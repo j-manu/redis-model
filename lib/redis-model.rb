541418e3dcf336363a7f0faa040b5e40c855361a
Alexey Metelev <alexey.metelev.2014@gmail.com>
2016-06-09 16:37:51 +0300

    Add JsonAttributes module
    This module adds Hash and Array attributes support to RedisModel::Base

--- lib/redis-model.rb
+++ lib/redis-model.rb
@@ -4,6 +4,7 @@ require 'active_support'
 # zones functionality is defined between versions
 require 'active_support/core_ext'
 require 'active_model'
-require "redis-model/version"
-require "redis-model/base"
 
+require "redis-model/version"
+require "redis-model/json_attributes.rb"
+require "redis-model/base"
